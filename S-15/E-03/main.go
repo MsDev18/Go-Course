7ebe5d61627f4058f14dd610d2edb36db085225b
MsDev18 <m.sadegh0086@gmail.com>
2025-11-15 03:48:14 +0330

    Watched S-15 E-03

---
+++ S-15/E-03/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"E-01/config"
+	"E-01/delivery/httpserver"
+	"E-01/repository/mysql"
+	"E-01/service/auth"
+	"E-01/service/user"
+	"E-01/validator/uservalidator"
+	"fmt"
+	"time"
+)
+
+const (
+	JwtSignKey                 = "jwt_secret"
+	AccessTokenSubject         = "at"
+	RefreshTokenSubject        = "rt"
+	AccessTokenExpireDuration  = time.Hour * 24
+	RefreshTokenExpireDuration = time.Hour * 24 * 7
+)
+
+func main() {
+	
+	cfg := config.Config{
+		HTTPServer: config.HTTPServer{Port: 8088},
+		Auth: auth.Config{
+			SignKey:               JwtSignKey,
+			AccessExpirationTime:  AccessTokenExpireDuration,
+			RefreshExpirationTime: RefreshTokenExpireDuration,
+			AccessSubject:         AccessTokenSubject,
+			RefreshSubject:        RefreshTokenSubject,
+		},
+		Mysql: mysql.Config{
+			Username: "gameapp",
+			Password: "gameappt0lk2o20",
+			Port:     3308,
+			Host:     "localhost",
+			DBName:   "gameapp_db",
+		},
+	}
+
+	// TODO - add command for migration
+	// mgr := migrator.New(cfg.Mysql)
+	// mgr.Up()
+
+	authSvc, userSvc, userValidator := setupServices(cfg)
+
+	server := httpserver.New(cfg, authSvc, userSvc, userValidator)
+
+	fmt.Println("Start Echo Server ...")
+	server.Serve()
+}
+
+func setupServices(cfg config.Config) (auth.Service, user.Service, uservalidator.Validator) {
+	MysqlRepo := mysql.New(cfg.Mysql)
+
+	uV := uservalidator.New(MysqlRepo)
+	authSvc := auth.New(cfg.Auth)
+	userSvc := user.New(authSvc, MysqlRepo)
+
+
+	return authSvc, userSvc ,uV
+}
---
+++ S-15/E-03/repository/mysql/user.go
@@ -0,0 +1,85 @@
+package mysql
+
+import (
+	"E-01/entity"
+	"E-01/pkg/errmsg"
+	"E-01/pkg/richerror"
+	"database/sql"
+	"fmt"
+	"time"
+)
+
+func (d *MySQLDB) IsPhoneNumberUnique(phoneNumber string) (bool, error) {
+	const op = "mysql.IsPhoneNumberUnique"
+
+	row := d.db.QueryRow(`select * from users where phone_number = ?`, phoneNumber)
+	_, err := scanUser(row)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return true, nil
+		}
+
+		return false, richerror.New(op).
+			WithErr(err).
+			WithMessage(errmsg.ErrMsgCantScanQueryResult).
+			WithKind(richerror.KindUnexpected)
+	}
+	return false, nil
+}
+
+func (d *MySQLDB) Register(u entity.User) (entity.User, error) {
+	res, err := d.db.Exec(`insert into users(name, phone_number, password) values(? , ?, ?);`, u.Name, u.PhoneNumber, u.Password)
+	if err != nil {
+		return entity.User{}, fmt.Errorf("can't execute command: %w", err)
+	}
+	// err is always nil
+	id, _ := res.LastInsertId()
+	u.ID = uint(id)
+	return u, nil
+}
+
+func (d *MySQLDB) GetUserByPhoneNumber(phoneNumber string) (entity.User, bool, error) {
+	const op = "mysql.GetUserByPhoneNumber"
+
+	row := d.db.QueryRow(`select * from users where phone_number = ?`, phoneNumber)
+	user, err := scanUser(row)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return entity.User{}, false, nil
+		}
+		return entity.User{}, false, richerror.New(op).
+			WithErr(err).
+			WithMessage(errmsg.ErrMsgCantScanQueryResult).
+			WithKind(richerror.KindUnexpected)
+	}
+	return user, true, nil
+}
+
+func (d *MySQLDB) GetUserByID(userID uint) (entity.User, error) {
+	const op = "mysql.GetUserByID"
+	row := d.db.QueryRow(`select * from users where id = ?`, userID)
+
+	user, err := scanUser(row)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return entity.User{}, richerror.New(op).
+				WithErr(err).
+				WithMessage(errmsg.ErrMsgNotFound).
+				WithKind(richerror.KindNotFound)
+		}
+		return entity.User{}, richerror.New(op).
+			WithErr(err).
+			WithMessage(errmsg.ErrMsgCantScanQueryResult).
+			WithKind(richerror.KindUnexpected)
+	}
+	return user, nil
+}
+
+func scanUser(row *sql.Row) (entity.User, error) {
+	var user entity.User
+	var createdAt time.Time
+
+	err := row.Scan(&user.ID, &user.Name, &user.PhoneNumber, &createdAt, &user.Password)
+
+	return user, err
+}
