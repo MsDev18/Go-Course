da346d9d80188858205e36665c12a91c14d85f2e
MsDev18 <m.sadegh0086@gmail.com>
2025-12-24 03:56:31 +0330

    fix matchin service

--- S-20/E-01/service/presenceservice/service.go
+++ S-20/E-01/service/presenceservice/service.go
@@ -39,3 +39,8 @@ func (s Service) Upsert(ctx context.Context, req param.UpsertPrecenseRequest) (p
 
 	return param.UpsertPresenceResponse{}, nil
 }
+
+
+func (s Service) GetPresence(ctx context.Context, request param.GetPresenceRequest)(param.GetPresenceResponse, error) {
+	return param.GetPresenceResponse{}, nil
+}
